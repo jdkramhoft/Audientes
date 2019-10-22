109f7d6991965a4c7844cb2d9dc3ad2cae4721cb
jdkramhoft <s175445@student.dtu.dk>
2019-10-22 07:51:00 +0200

    inital commit

---
+++ app/src/main/java/a3/audientes/MainActivity.java
@@ -0,0 +1,14 @@
+package a3.audientes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
