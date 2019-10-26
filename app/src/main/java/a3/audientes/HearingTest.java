a8d1047bfd6a2d43ebe7d0d58e0129b1282f5e1b
s175565 <s175565@student.dtu.dk>
2019-10-26 12:45:55 +0200

    hearing test empty activity

---
+++ app/src/main/java/a3/audientes/HearingTest.java
@@ -0,0 +1,14 @@
+package a3.audientes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HearingTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hearing_test);
+    }
+}
