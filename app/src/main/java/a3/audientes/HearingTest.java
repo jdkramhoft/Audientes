9803ee66d38fddf11335e7854fcd3321eef9449a
s175565 <s175565@student.dtu.dk>
2019-10-27 10:52:46 +0100

    buttons design

--- app/src/main/java/a3/audientes/HearingTest.java
+++ app/src/main/java/a3/audientes/HearingTest.java
@@ -3,12 +3,36 @@ package a3.audientes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HearingTest extends AppCompatActivity {
+public class HearingTest extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mHeardSoundBtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hearing_test);
+
+        mHeardSoundBtn = findViewById(R.id.heard_sound_btn);
+        mHeardSoundBtn.setOnClickListener(this);
+
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == mHeardSoundBtn){
+
+
+
+
+            // TODO: call heard_sound method from interface
+        }
     }
 }
--- app/src/main/java/a3/audientes/OnboardingActivity.java
+++ app/src/main/java/a3/audientes/OnboardingActivity.java
@@ -72,7 +72,7 @@ public class OnboardingActivity extends AppCompatActivity implements View.OnClic
             if (lastPage){
                 launchHearingTestScreen();
             }
-            else{
+            else {
                 mSlideViewPager.setCurrentItem(mCurrentPage + 1);
             }
         }
@@ -84,7 +84,7 @@ public class OnboardingActivity extends AppCompatActivity implements View.OnClic
     private void launchHearingTestScreen(){
         Utils.saveSharedSetting(OnboardingActivity.this, MenuActivity.PREF_USER_FIRST_TIME, "false");
 
-        Intent hearingTestIntent = new Intent(this, HearingTest.class);
+        Intent hearingTestIntent = new Intent(this, BeginHearingTestActivity.class);
         hearingTestIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(hearingTestIntent);
         finish();
