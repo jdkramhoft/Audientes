e2056f3a81dc0342608ad056a5f516c6e682e488
Marina <marina@mallingnet.dk>
2019-10-28 22:05:47 +0100

    updates

--- app/src/main/java/a3/audientes/Programs.java
+++ app/src/main/java/a3/audientes/Programs.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 
 public class Programs extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton noisy_edit, normal_edit, cinema_edit, quiet_edit;
+    CheckBox noisy_check, normal_check, cinema_check, quiet_check;
 
 
     @Override
@@ -30,6 +32,18 @@ public class Programs extends AppCompatActivity implements View.OnClickListener 
         quiet_edit = findViewById(R.id.quiet_edit);
         quiet_edit.setOnClickListener(this);
 
+        noisy_check = findViewById(R.id.noisy_check);
+        noisy_check.setOnClickListener(this);
+
+        normal_check = findViewById(R.id.normal_check);
+        normal_check.setOnClickListener(this);
+
+        cinema_check = findViewById(R.id.cinema_check);
+        cinema_check.setOnClickListener(this);
+
+        quiet_check = findViewById(R.id.quiet_check);
+        quiet_check.setOnClickListener(this);
+
     }
 
     @Override
@@ -47,6 +61,15 @@ public class Programs extends AppCompatActivity implements View.OnClickListener 
         } else if ( v == quiet_edit) {
             Intent intent = new Intent(this, EditProgram.class);
             startActivity(intent);
+
+        } else if (v == noisy_check) {
+            //TODO
+        } else if ( v == normal_check) {
+            //TODO
+        } else if ( v == cinema_check) {
+            //TODO
+        } else if ( v == quiet_check) {
+            //TODO
         }
 
     }
