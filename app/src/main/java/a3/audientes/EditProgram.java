b5f361ef71e8f2255f2831850e95a611b28659e5
Marina <marina@mallingnet.dk>
2019-11-26 16:46:17 +0100

    progress bar w/ color done

--- app/src/main/java/a3/audientes/EditProgram.java
+++ app/src/main/java/a3/audientes/EditProgram.java
@@ -3,12 +3,135 @@ package a3.audientes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class EditProgram extends AppCompatActivity {
+public class EditProgram extends AppCompatActivity implements View.OnClickListener {
+
+    private SeekBar low_plus, low, medium, high, high_plus;
+    private TextView set_one, set_two, set_three, set_four, set_five;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_program);
+
+        set_one = findViewById(R.id.set_one);
+        set_two = findViewById(R.id.set_two);
+        set_three = findViewById(R.id.set_three);
+        set_four = findViewById(R.id.set_four);
+        set_five = findViewById(R.id.set_five);
+
+        low_plus = findViewById(R.id.low_plus);
+        low_plus.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                set_one.setText("" + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+
+            }
+        });
+
+        low = findViewById(R.id.low);
+        low.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                set_two.setText("" + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+
+            }
+        });
+
+        medium = findViewById(R.id.medium);
+        medium.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                set_three.setText("" + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        high = findViewById(R.id.high);
+        high.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                set_four.setText("" + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        high_plus = findViewById(R.id.high_plus);
+        high.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                set_five.setText("" + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+
+
+
+
 }
