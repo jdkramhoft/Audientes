f55d1f70712f134aab5bb2ed4e02575267c59b6f
jdkramhoft <s175445@student.dtu.dk>
2019-11-28 13:25:21 +0100

    SetStatusBarTransparent method updated to reflect the fact that our minSDK is now 21+

--- app/src/main/java/a3/audientes/activities/Launcher.java
+++ app/src/main/java/a3/audientes/activities/Launcher.java
@@ -54,17 +54,9 @@ public final class Launcher extends AppCompatActivity {
 
     //TODO HALP IT WONT BE TRANSPARTEN
     public void setStatusBarTrans(){
-        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
-        }
-        if (Build.VERSION.SDK_INT >= 19) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-        //make fully Android Transparent Status bar
-        if (Build.VERSION.SDK_INT >= 21) {
-            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
     }
 
     public static void setWindowFlag(Activity activity, final int bits, boolean on) {
