e1baff40d20e599adf26fdbb5f44eac88f6cbd78
jarberg <mjarberg88@gmail.com>
2020-01-16 12:06:47 +0100

    added is connected check on splash screen for devices

--- app/src/main/java/a3/audientes/view/activities/SplashScreen.java
+++ app/src/main/java/a3/audientes/view/activities/SplashScreen.java
@@ -2,14 +2,28 @@ package a3.audientes.view.activities;
 
 
 import android.Manifest;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.ParcelUuid;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.ViewModelProviders;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import a3.audientes.R;
 import a3.audientes.dao.AudiogramDAO;
@@ -17,6 +31,10 @@ import a3.audientes.dao.ProgramDAO;
 import a3.audientes.viewmodel.AudiogramViewModel;
 import a3.audientes.viewmodel.ProgramViewModel;
 import a3.audientes.utils.SharedPrefUtil;
+
+import static android.bluetooth.BluetoothProfile.GATT;
+import static android.bluetooth.BluetoothProfile.GATT_SERVER;
+import static android.bluetooth.BluetoothProfile.HEADSET;
 
 public final class SplashScreen extends AppCompatActivity {
 
@@ -32,7 +50,7 @@ public final class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
-
+        isHearableConnected();
         if (savedInstanceState == null){
             // TODO: only onboarding on first visit
             handler.postDelayed(splash, 2000);
@@ -54,8 +72,28 @@ public final class SplashScreen extends AppCompatActivity {
 
     private boolean isHearableConnected() {
         // TODO: somehow check if the correct device is already connected
+        BluetoothManager manager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+
+        List<BluetoothDevice> connected = new ArrayList<>();
+
+        for (BluetoothDevice e: manager.getAdapter().getBondedDevices()) {
+            if(isConnected(e)){
+                return true;
+            }
+        }
         return false;
     }
+
+    public static boolean isConnected(BluetoothDevice device) {
+        try {
+            Method m = device.getClass().getMethod("isConnected", (Class[]) null);
+            boolean connected = (boolean) m.invoke(device, (Object[]) null);
+            return connected;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
 
     private final Runnable splash = () -> {
         assert getFragmentManager() != null;
