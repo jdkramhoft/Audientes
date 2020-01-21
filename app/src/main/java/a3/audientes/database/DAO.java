24622133bc86e379954b66883be26b06b4ed783a
jdkramhoft <s175445@student.dtu.dk>
2020-01-21 15:16:44 +0100

    Opdelt interfaces

--- app/src/main/java/a3/audientes/database/DAO.java
+++ app/src/main/java/a3/audientes/database/DAO.java
@@ -1,51 +1,7 @@
 package a3.audientes.database;
 
-import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
-import androidx.room.Delete;
-import androidx.room.Insert;
-import androidx.room.Query;
-import androidx.room.Update;
-
-import java.util.List;
-
-import a3.audientes.dto.Audiogram;
-import a3.audientes.dto.Program;
 
 @Dao
-public interface DAO {
-
-    // Program
-
-    @Insert
-    void insertProgram(Program program);
-
-    @Update
-    void updateProgram(Program program);
-
-    @Delete
-    void deleteProgram(Program program);
-
-    @Query("DELETE FROM program_table WHERE id = :ID")
-    void deleteProgramWithID(int ID);
-
-    @Query("SELECT * FROM program_table")
-    LiveData<List<Program>> getAllPrograms();
-
-    // Audiogram
-
-    @Insert
-    void insertAudiogram(Audiogram audiogram);
-
-    @Update
-    void updateAudiogram(Audiogram audiogram);
-
-    @Delete
-    void deleteAudiogram(Audiogram audiogram);
-
-    @Query("DELETE FROM audiogram_table WHERE id = :ID")
-    void deleteAudiogramWithID(int ID);
-
-    @Query("SELECT * FROM audiogram_table")
-    LiveData<List<Audiogram>> getAllAudiogram();
+public interface DAO extends AudiogramDAO, ProgramDAO{
 }
