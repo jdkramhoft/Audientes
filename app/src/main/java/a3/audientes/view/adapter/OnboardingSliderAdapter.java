7d617d046f354c4e3218f12eb0582c8ec5fcc0ad
jarberg <mjarberg88@gmail.com>
2020-01-22 17:36:41 +0100

    translated static english tekst to english, danish and hindu

--- app/src/main/java/a3/audientes/view/adapter/OnboardingSliderAdapter.java
+++ app/src/main/java/a3/audientes/view/adapter/OnboardingSliderAdapter.java
@@ -32,21 +32,21 @@ public class OnboardingSliderAdapter extends PagerAdapter {
     };
 
     private String[] slide_headings = {
-            "Welcome to Audientes",
-            "Your surroundings",
-            "Let's get started",
-            "How to delete programs",
-            "How to edit a program",
-            "How to start a Hearing Test",
+            context.getString(R.string.slidehead1),
+            context.getString(R.string.slidehead2),
+            context.getString(R.string.slidehead3),
+            context.getString(R.string.slidehead4),
+            context.getString(R.string.slidehead5),
+            context.getString(R.string.slidehead6)
     };
 
     private String[] slide_descs = {
-            "Your hearable will work optimally when you perform a hearing test using the built-in Pure Tone Audiometry, just like when visiting a hearing specialist. ",
-            "Before starting the hearing test, we check the surrounding noise level, if it is greater than expected value, we would suggest that the user find another quiet place.",
-            "You will be asked to wear the hearable and sit in a quiet area free for disturbing sounds and other factors. A series of sounds will be broadcast through the earphones. It will measure your ability to hear sounds at various pitches and volumes. ",
-            "When deleting a program, you will need to click on the 'x' button in order to delete it. If a program does not have a 'x' in the upper right corner, it means that it is a default program and cannot be deleted.",
-            "In order to edit a program, a default or custom program, you will need to click and hold the button - otherwise you will only select the program.",
-            "In order to be able to start the hearing test, you'll need to allow this application to use the mic, because the upper right corner display the sound level and only if it is green you can take the hearing test.",
+            context.getString(R.string.slidedesc1),
+            context.getString(R.string.slidedesc2),
+            context.getString(R.string.slidedesc3),
+            context.getString(R.string.slidedesc4),
+            context.getString(R.string.slidedesc5),
+            context.getString(R.string.slidedesc6)
     };
 
     public OnboardingSliderAdapter(Context context){
